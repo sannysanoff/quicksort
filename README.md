ee7dd82129f3016d5a75faac212adc1d3a53d77c
Sanny Sanoff <san@sysdate.com>
2019-07-19 01:09:47 +0300

    fix d

---
+++ README.md
@@ -0,0 +1,6 @@
+
+c++ 5.2 sec
+
+swift 6.1 sec
+
+d  8 sec
---
+++ d/source/app.d
@@ -0,0 +1,61 @@
+import std.stdio;
+import core.time;
+import std.datetime.stopwatch;
+import std.random;
+import std.algorithm.sorting;
+import std.algorithm.mutation;
+
+struct P { double a; double b; }
+alias myComp = (ref P x, ref P y) => x.a > y.a || x.a == y.a && x.b > y.b;
+
+
+long partition (P[] arr, long low, long high)
+{
+    P pivot = arr[high];    // pivot
+    auto i = (low - 1);  // Index of smaller element
+
+    for (auto j = low; j <= high- 1; j++)
+    {
+        // If current element is smaller than or
+        // equal to pivot
+        if (!myComp(arr[j], pivot))
+        {
+            i++;    // increment index of smaller element
+            swap(arr[i], arr[j]);
+        }
+    }
+    swap(arr[i + 1], arr[high]);
+    return (i + 1);
+}
+
+void quickSort(P[] arr, long low, long high) {
+    if (low < high)
+    {
+        /* pi is partitioning index, arr[p] is now
+           at right place */
+        long pi = partition(arr, low, high);
+
+        // Separately sort elements before
+        // partition and after partition
+        quickSort(arr, low, pi - 1);
+        quickSort(arr, pi + 1, high);
+    }
+}
+
+void main() {
+    P[] arr;
+    auto rnd = Random(1000);
+    writeln("generating..."); stdout.flush();
+    auto n = 50_000_000;
+    for(int q=0; q<n; q++) {
+        arr ~= P(uniform(0.0f, 1.0f, rnd), uniform(0.0f, 1.0f, rnd));
+    }
+    auto sw = StopWatch(AutoStart.no);
+    writeln("sorting..."); stdout.flush();
+    sw.start();
+//    auto q = arr.sort!(myComp);
+    quickSort(arr, 0, arr.length-1);
+    sw.stop();
+
+    writeln(sw.peek.total!"msecs");
+}
