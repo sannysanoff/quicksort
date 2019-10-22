3c3667aee022a5f59afda673ab5d70bcfa7cd28b
Sanny Sanoff <san@sysdate.com>
2019-10-22 22:38:26 +0000

    recent changes

---
+++ c++/run_lli.sh
@@ -0,0 +1,1 @@
+graalvm-ee-19.2.1/bin/lli --lib /usr/lib/x86_64-linux-gnu/libstdc++.so.6 main2.bc
--- d/build.sh
+++ d/build.sh
@@ -1,3 +1,3 @@
 dmd -of=app_dmd -O -release -inline -boundscheck=off source/app.d
-ldc -of=app_ldc -O3 -boundscheck=off -release source/app.d
+ldc2 -of=app_ldc -O3 -boundscheck=off -release source/app.d
 
